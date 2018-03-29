b038e942fe0a69703e30465414bf042071bfda6c
vikassinghwebsp <36074752+vikassinghwebsp@users.noreply.github.com>
2018-03-29 16:30:33 +0530

    default databse config file

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,79 @@
+<?php return array (
+  'intervention/image' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Intervention\\Image\\ImageServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Image' => 'Intervention\\Image\\Facades\\Image',
+    ),
+  ),
+  'gloudemans/shoppingcart' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Gloudemans\\Shoppingcart\\ShoppingcartServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Cart' => 'Gloudemans\\Shoppingcart\\Facades\\Cart',
+    ),
+  ),
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'maatwebsite/excel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Maatwebsite\\Excel\\ExcelServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Excel' => 'Maatwebsite\\Excel\\Facades\\Excel',
+    ),
+  ),
+  'cartalyst/stripe-laravel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Cartalyst\\Stripe\\Laravel\\StripeServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Stripe' => 'Cartalyst\\Stripe\\Laravel\\Facades\\Stripe',
+    ),
+  ),
+  'dimsav/laravel-translatable' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Dimsav\\Translatable\\TranslatableServiceProvider',
+    ),
+  ),
+  'laravelcollective/html' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Collective\\Html\\HtmlServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Form' => 'Collective\\Html\\FormFacade',
+      'Html' => 'Collective\\Html\\HtmlFacade',
+    ),
+  ),
+);
---
+++ config/variables.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ * Name : Variables
+ * Description : Define all containt variables here.
+ * Authour : Sunny Saini <sunnysaini466@gmail.com>
+ */
+
+return [
+    'tbl_admin' => 'tbl_admin',
+    'tbl_invoice_address' => 'tbl_invoice_address',
+    'tbl_menu' => 'tbl_menu',
+    'tbl_submenu' => 'tbl_submenu',
+    'tbl_cat' => 'tbl_category',
+    'tbl_subcat' => 'tbl_subcategory',
+    'tbl_subsubcat' => 'tbl_subsubcategory',
+    'tbl_product_img' => 'tbl_product_images',
+    'tbl_banner' => 'tbl_banner',
+    'tbl_products' => 'tbl_product',
+    'tbl_aboutus' => 'tbl_aboutus',
+    'tbl_address' => 'tbl_address',
+    'tbl_social_media' => 'tbl_social_media',
+    'tbl_terms' => 'tbl_terms',
+    'tbl_banner_name' => 'tbl_banner_name',
+    'tbl_brands' => 'tbl_brands',
+    'tbl_orders' => 'tbl_orders',
+    'tbl_countries' => 'tbl_countries',
+    'tbl_frontcat' => 'tbl_frontcat',
+    'products_img' => '/assets/images/products/',
+    'products_img_thumb' => '/assets/images/products/thumb',
+    'products_img_thumb213x215' => '/assets/images/products/213x215',
+    'products_img_thumb276x205' => '/assets/images/products/276x205',
+    'products_img_thumb380x200' => '/assets/images/products/380x200',
+    
+    
+    
+    
+    
+    //email
+    'mail_from'=>'mailtosunnysaini@gmail.com',
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    'tbl_city' => 'ct_city',
+    'tbl_citycoupon' => 'ct_citycoupon',
+    'tbl_storecategory' => 'ct_storecategory',
+    'tbl_couponsubcategory' => 'ct_couponsubcategory',
+    'tbl_subscribe' => 'ct_subscribe',
+    'admin_avatar' => '/backend/images/avatar',
+    'admin_title' => ' Coupon Code Backend',
+    'site_title' => ' Coupont Code',
+    'store_thum_img' => '/images/store/thumb/',
+    'store_img' => '/images/store/',
+    
+    'aboutus_img' => '/assets/images/aboutus/',
+    'banner_img' => '/assets/images/banner/',
+    'banner_img_thumb' => '/assets/images/banner/thumb/',
+    'brand_img' => '/assets/images/brands/',
+    
+];
